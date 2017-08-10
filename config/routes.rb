4365bbe2f6a0728652f7b38f9005361d158d64ef
富永治明 <tominagaharuaki@MacBook-Pro.local>
2017-08-10 16:40:06 +0900

    level2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
   Rails.application.routes.draw do
-  devise_for :users
     root  'tweets#index'                       #ルートパスの指定
     get   'tweets'      =>  'tweets#index'     #ツイート一覧画面
     get   'tweets/new'  =>  'tweets#new'       #ツイート投稿画面
--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20170809123307) do
+ActiveRecord::Schema.define(version: 20170809112935) do
 
   create_table "tweets", force: :cascade do |t|
     t.string   "name",       limit: 255
