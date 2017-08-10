eed6435291efe5ca854d89c218f86b7be48e2bbb
富永治明 <tominagaharuaki@MacBook-Pro.local>
2017-08-10 17:12:08 +0900

    4-5

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,19 +1,24 @@
-class TweetsController < ApplicationController
+  class TweetsController < ApplicationController
 
-  def index
-    @tweets = Tweet.order("created_at DESC").page(params[:page]).per(5)
+    before_action :move_to_index, except: :index
+
+    def index
+      @tweets = Tweet.page(params[:page]).per(5).order("created_at DESC")
+    end
+
+    def new
+    end
+
+    def create
+      Tweet.create(tweet_params)
+    end
+
+    private
+    def tweet_params
+      params.permit(:name, :image, :text)
+    end
+
+    def move_to_index
+      redirect_to action: :index unless user_signed_in?
+    end
   end
-
-  def new
-  end
-
-  def create
-    Tweet.create(tweet_params)
-  end
-
-  private
-  def tweet_params
-    params.permit(:name, :image, :text)
-  end
-
-end
